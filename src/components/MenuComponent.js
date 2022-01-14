c1f24cb0d258ac6c976ded3a72f56d5029ee074b
congly <daocongly0510@gmail.com>
2022-01-14 21:27:59 +0700

    tao mo component basic

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Bo thuong con gai len bo se co gang</h1>
+
+        );
+    }
+}
+export default Menu;
