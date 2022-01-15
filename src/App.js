5379cfaf1fd3902575b61dfd3d3d2d64adff83c2
congly <daocongly0510@gmail.com>
2022-01-16 02:07:56 +0700

    đã thêm các file cần thiết

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {STAFFS} from './share/staffs';
 
 function App() {
   return (
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+class StaffList extends React.Component {
+    constructor(props) {
+
+    }
+    render() { return (
+        
+
+    );
+
+
+    }
+}
