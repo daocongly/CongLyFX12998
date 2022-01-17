cdab1365183c1630dbf174549d806b7d8a205eb2
congly <daocongly0510@gmail.com>
2022-01-17 20:43:56 +0700

    prop componen parent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 // import './App.css';
+import {STAFFS} from './share/staffs';
 import StaffList from './components/StaffListComponent';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staff: STAFFS
+    }
+  }
   render (){
     return (
       <>
       <div className="p-3 h2 bg-primary text-white ">Ứng dụng quản lý nhân sự v1.0</div>
       <div class="container-fluid">              
-              <StaffList/>
+              <StaffList staff={this.state.staff}/>
       </div>
 
       </>
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -9,11 +9,11 @@ class StaffList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            staffslist: STAFFS};
+           };
 
     }
     render(){
-        const staff=this.state.staffslist.map((staff) =>{
+        const staff=this.props.staffslist.map((staff) =>{
             return (
                 
                     <div className="col-sm-12 col-md-5 col-lg-3 card-header m-1">{staff.name}</div>
