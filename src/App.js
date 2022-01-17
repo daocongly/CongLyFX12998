5b853a57ea638aad620044e3331cb0335f686360
congly <daocongly0510@gmail.com>
2022-01-17 16:40:34 +0700

    giao dien ok

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-// import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 import React from 'react';
 // import './App.css';
-import {STAFFS} from './share/staffs';
+import StaffList from './components/StaffListComponent';
+
 
 class App extends React.Component {
   render (){
     return (
       <>
       <div className="p-3 h2 bg-primary text-white ">Ứng dụng quản lý nhân sự v1.0</div>
-      <div class="container">      
-            <div className="row">
-                <div className="col-sm-12 col-md-6 col-lg-4">
-                <div className="card-header m-2">Header</div>
-                <div className="card-header m-2">Content</div> 
-                <div className="card-header m-2">Footer</div>
-                   
-                </div>
-                <div className="col-sm-12 col-md-6 col-lg-4">
-                <div className="card-header m-2">Header</div>
-                <div className="card-header m-2">Content</div> 
-                <div className="card-header m-2">Footer</div>
-
-                </div>
-                <div className="col-sm-12 col-md-6 col-lg-4">
-                <div className="card-header m-2">Header</div>
-                <div className="card-header m-2">Content</div> 
-                <div className="card-header m-2">Footer</div>
-
-
-                </div>
-            </div>
-            <p>Bấm vào tên nhân viên để xem thông tin</p>
-
-        </div>
+      <div class="container-fluid">              
+              <StaffList/>
+      </div>
 
       </>
       
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,21 +1,38 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+
 import dateFormat from 'dateformat';
+import { STAFFS } from '../share/staffs';
 
 
 class StaffList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {props};
+        this.state = {
+            staffslist: STAFFS};
 
     }
-    render() { return (
-        <div class="container-fluid">      
-            
-        </div>
+    render(){
+        const staff=this.state.staffslist.map((staff) =>{
+            return (
+                
+                    <div className="col-sm-12 col-md-5 col-lg-3 card-header m-1">{staff.name}</div>
+                
+            );
+        })
 
-    );
-
+        return (
+            <div className="row" >      
+                {staff}
+                <p>Bấm vào tên nhân viên để xem thông tin</p>
+            </div>
+    
+        );
 
     }
+    
+
+
+    
 }
+export default StaffList;
