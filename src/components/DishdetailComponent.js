02fe1d4dd1899262a67984cbce27f6882f2c8f48
congly <daocongly0510@gmail.com>
2022-02-13 01:53:20 +0700

    copy lab04_1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import dateFormat from 'dateformat';
+
+
+class DishDetail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    renderComment(detail) {
+        const nhanXet = detail.comments.map((nhanxet) =>
+        { return(
+            <>
+            <p>{nhanxet.comment}</p>
+            <p>{nhanxet.author}, {dateFormat(nhanxet.date,"mmm d, yyyy")} </p>
+            </>
+            );
+        });
+        return( nhanXet );
+
+    }
+
+    renderDish(detail) {
+        if (detail!=null) {
+            return (
+                <div className="container ">
+                    <div className="row">
+                <div className="col-sm-12 col-md-5 m-1">
+                    <Card>
+                    <CardImg with="100%" object src={detail.image} alt={detail.name}/>
+                    <CardBody>
+                    <CardTitle className="h3">{detail.name}</CardTitle>
+                    <CardText>{detail.description}</CardText>
+                    </CardBody>
+                     </Card>
+
+                </div>
+                <div className="col-sm-12 col-md-5 m-1" >
+                 {this.renderComment(this.props.dish)}
+                </div >
+                </div>
+                </div>
+                );           
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+    render() {
+        return(
+        <>
+        {this.renderDish(this.props.dish)}
+        </>
+        );
+        
+    }
+}
+export default DishDetail;
---
+++ src/components/MenuComponent.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);        
+    }
+   
+    render() {
+        const menu =this.props.dishes.map((dish)=> {
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=>this.props.onClick(dish.id)}>
+                        <CardImg with="100%" object src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay>
+                            <CardTitle className="h3">{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+        return (
+            <div className="container">
+                <div className="row">
+                        {menu}
+                </div>              
+            </div>
+
+        );
+    }
+}
+export default Menu;
