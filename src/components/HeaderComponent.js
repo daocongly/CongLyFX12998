a932613cd1de7925844d73deba86b86b7a915d17
congly <daocongly0510@gmail.com>
2022-02-16 10:25:30 +0700

    complete Header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Navbar, NavbarBrand, Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import { Jumbotron } from 'react-bootstrap';
 
 
 class Header extends React.Component {
@@ -11,17 +12,15 @@ class Header extends React.Component {
                     <div className="container">
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
-                </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
+                </Navbar>              
+                    <div className="container-fluid">
+                        <div className="row row-header mt-4 p-5 bg-secondary  text-white rounded ">
+                            <div className="col-12 col-sm-6 ">
                                 <h1>Ristorante Con Fusion </h1>
                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
-                    </div>
-                </Jumbotron>
+                    </div>              
             </>
         )
     }
