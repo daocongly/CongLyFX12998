f9b67a7487e32fa96b3cbc5add73c08ad8a18a58
congly <daocongly0510@gmail.com>
2022-02-17 17:00:01 +0700

    13.20

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Routes, Route, useParams} from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
@@ -14,15 +14,21 @@ class Main extends Component {
         dishes: DISHES,
     };
   }
-
+  
   render() {
+    const Homepage = () => {
+      return (
+        <Home/>
+      );
+    }
     return (
       <div>
         <Header/>
-        <Switch>
-          <Route path="/home" component={Homepage} />
-          <Route exact path="/menu" component={Menu} />
-        </Switch>
+        <Routes>
+          <Route path="/home" element={<Homepage/>} />
+          <Route path="/" element={<Homepage/>} />
+          <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />}/>
+        </Routes>
         <Footer/>
       </div>
     );
