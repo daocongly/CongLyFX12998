363c1874af025fb21e7ff1b4a16e196ee868fe0b
congly <daocongly0510@gmail.com>
2022-03-20 22:39:57 +0700

    copy lab05_3

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import dateFormat from 'dateformat';
+
+    function RenderComment({dishComment}) {
+        const listComment = dishComment.map((nhanxet) => {
+            return (
+                <>
+                <p>{nhanxet.comment}</p>
+                <p>{nhanxet.author}, {dateFormat(nhanxet.date,"mmm d, yyyy")} </p>
+                </>
+            );
+        });
+        return listComment;
+    }
+
+    function RenderDish({dish}) {
+        if (dish != null) {
+            return(
+            <div className="container ">
+                <div className="row">
+                    <div className="col-sm-12 col-md-5 m-1">
+
+                        <Card>
+                        <CardImg with="100%" object src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                        <CardTitle className="h3">{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        </CardBody>
+                        </Card>
+
+                    </div>
+                    <div className="col-sm-12 col-md-5 m-1" >
+                        <RenderComment dishComment={dish.comments}/>
+                    </div>
+                </div>
+            </div>
+
+            );
+        }
+        else {
+            return <div></div>                     
+        }
+
+    }
+
+    const DishDetail = (props) => {
+
+    return (  
+        <RenderDish dish = {props.dish} />       
+    );
+    }
+
+export default DishDetail;
