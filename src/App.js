9ae5d7a920d737955cedf56af8d06d678ad8c940
congly <daocongly0510@gmail.com>
2022-04-18 08:52:41 +0700

    copy bai Assign 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+// import './App.css';
+import {STAFFS} from './share/staffs';
+import StaffList from './components/StaffListComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staff: STAFFS
+    }
+  }
+  render (){
+    return (
+      <>
+      <div className="p-3 h2 bg-primary text-white ">Ứng dụng quản lý nhân sự v1.0</div>
+      <div class="container-fluid">              
+              <StaffList staff={this.state.staff}/>
+      </div>
+
+      </>
+      
+
+    )
+  }
 }
 
 export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,75 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import dateFormat, {masks} from 'dateformat';
+import { STAFFS } from '../share/staffs';
+
+
+class StaffList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectednhanvien: null
+           };
+
+    }
+    nhanVien(staff) {
+        this.setState({selectednhanvien: staff});
+
+    }
+
+    rendernhanvien(staff) {
+        if (staff !=null){
+            return (
+                <div className="card">
+                    <h2>Họ và tên:{staff.name} </h2>
+                    <p>Ngày sinh: {dateFormat(staff.doB,"dd/mm/yyyy")}</p>
+                    <p>Ngày vào công ty: {dateFormat(staff.startDate,"dd/mm/yyyy")}</p>
+                    <p>Phòng ban: {staff.department.name}</p>
+                    <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+                    <p>Số ngày đã làm thêm: {staff.overTime}</p>
+                </div>
+
+            );
+
+        }
+        else {
+            return(
+                <div></div>
+
+            );
+        }
+    }
+
+
+
+    render(){
+        const staffs=this.props.staff.map((staff) =>{
+            return (
+                
+                    <div onClick={() => this.nhanVien(staff)} className="col-sm-12 col-md-5 col-lg-3 card-header m-1">{staff.name}</div>
+                
+            );
+        })
+
+        return (
+            <>
+            <div className="row" >      
+                {staffs}  
+            </div>
+            <div className="row ">
+                {this.rendernhanvien(this.state.selectednhanvien)}
+
+            </div>
+            </>
+            
+    
+        );
+
+    }
+    
+
+
+    
+}
+export default StaffList;
