cac57d4fae4ec7bc52b10b2239053485acc9792b
congly <daocongly0510@gmail.com>
2022-04-18 12:39:16 +0700

    complete Header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -30,37 +30,24 @@ class Header extends React.Component {
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
+                                    <NavLink className="nav-link" to="/nhanvien">
+                                    <i class="fa fa-users" aria-hidden="true"></i> Nhân Viên
                                     </NavLink>    
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span>About Us
+                                    <NavLink className="nav-link" to="/phongban">
+                                    <i class="fa fa-id-card" aria-hidden="true"></i> Phòng Ban
                                     </NavLink>   
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span>Menu
+                                    <NavLink className="nav-link" to="/bangluong">
+                                    <i class="fa fa-money" aria-hidden="true"></i> Bảng Lương
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact Us
-                                    </NavLink>
-                                </NavItem>
+                                </NavItem>                               
                             </Nav>
                         </Collapse>
                     </div>
-                </Navbar>              
-                    <div className="container-fluid fixsai">
-                        <div className="row jumbotron ">
-                            <div className="col-12 col-sm-6 ">
-                                <h1>Ristorante Con Fusion </h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                            </div>
-                        </div>
-                    </div>              
+                </Navbar>                                           
             </>
         )
     }
