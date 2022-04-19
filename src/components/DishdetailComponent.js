fa6bfa08d6d55df38761d54e4160782012da9086
congly <daocongly0510@gmail.com>
2022-04-19 16:13:27 +0700

    chinh sua cho dep

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,55 +4,6 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import dateFormat from 'dateformat';
 import {Link} from 'react-router-dom';
 
-
-    // function RenderComment({dishComment}) {
-    //     const listComment = dishComment.map((nhanxet) => {
-    //         return (
-    //             <>
-    //             <p>{nhanxet.comment}</p>
-    //             <p>{nhanxet.author}, {dateFormat(nhanxet.date,"mmm d, yyyy")} </p>
-    //             </>
-    //         );
-    //     });
-    //     return listComment;
-    // }
-
-    // function RenderDish({dish}) {
-    //     if (dish != null) {
-    //         return(
-    //         <div className="container ">
-    //             <div className="row">
-    //                 <div className="col-sm-12 col-md-5 m-1">
-
-    //                     <Card>
-    //                     <CardImg with="100%" object src={dish.image} alt={dish.name}/>
-    //                     <CardBody>
-    //                     <CardTitle className="h3">{dish.name}</CardTitle>
-    //                     <CardText>{dish.description}</CardText>
-    //                     </CardBody>
-    //                     </Card>
-
-    //                 </div>
-    //                 <div className="col-sm-12 col-md-5 m-1" >
-    //                     <RenderComment dishComment={dish.comments}/>
-    //                 </div>
-    //             </div>
-    //         </div>
-
-    //         );
-    //     }
-    //     else {
-    //         return <div></div>                     
-    //     }
-
-    // }
-
-    // const DishDetail = (props) => {
-
-    // return (  
-    //     <RenderDish dish = {props.dish} />       
-    // );
-    // }
 function RenderDish({dish}) {
     return (
         <div className="col-12 col-md-5 m-1">
@@ -114,8 +65,5 @@ const DishDetail = (props) => {
         <div></div>
     );
 }
-
-
-
 
 export default DishDetail;
