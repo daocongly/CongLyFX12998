0974e02c3fb7a5b320a3d9aa73814c33585c9ba8
congly <daocongly0510@gmail.com>
2022-05-06 20:44:48 +0700

    hoan thanh

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -15,11 +15,24 @@ class Contact extends React.Component {
             message: '',
 
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+
     }
     handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
 
     }
     handleSubmit(event) {
+        console.log("Current State is:" + JSON.stringify(this.state));
+        alert("Current State is:" + JSON.stringify(this.state));
+        event.preventDefault();
+        
 
     }
 
@@ -73,7 +86,8 @@ class Contact extends React.Component {
                                 <Col md={10}>
                                     <Input type="text" id="firstname" name="firstname"
                                     placeholder="First Name"
-                                    value={this.state.first}/>
+                                    value={this.state.first}
+                                    onChange={this.handleInputChange}/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -81,7 +95,8 @@ class Contact extends React.Component {
                                 <Col md={10}>
                                     <Input type="text" id="lastname" name="lastname"
                                     placeholder="Last Name"
-                                    value={this.state.lastname}/>
+                                    value={this.state.lastname}
+                                    onChange={this.handleInputChange}/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -89,7 +104,8 @@ class Contact extends React.Component {
                                 <Col md={10}>
                                     <Input type="tel" id="telnum" name="telnum"
                                     placeholder="Tel. Number"
-                                    value={this.state.telnum}/>
+                                    value={this.state.telnum}
+                                    onChange={this.handleInputChange}/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -97,7 +113,8 @@ class Contact extends React.Component {
                                 <Col md={10}>
                                     <Input type="email" id="email" name="email"
                                     placeholder="Email"
-                                    value={this.state.email}/>
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -105,13 +122,15 @@ class Contact extends React.Component {
                                 <FormGroup check>
                                     <Label check>
                                         <Input type="checkbox" name="agree"
-                                            checked ={this.state.agree}/>{' '}
+                                            checked ={this.state.agree}
+                                            onChange={this.handleInputChange}/>{' '}
                                         <strong>May we contact you?</strong>
                                     </Label>
                                 </FormGroup>
                                 <Col md={{size: 3,offset: 1}}>
                                 <Input type="select" name="contactType"
-                                    value={this.state.contactType}>
+                                    value={this.state.contactType}
+                                    onChange={this.handleInputChange}>
                                     <option>Tel.</option>
                                     <option>Email</option>
                                 </Input>
@@ -122,7 +141,8 @@ class Contact extends React.Component {
                                 <Col md={10}>
                                     <Input type="textarea" id="message" name="message"
                                     rows ="12"
-                                    value={this.state.message}/>
+                                    value={this.state.message}
+                                    onChange={this.handleInputChange}/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
