5cc5182f9e578b252fb7fb42cc28d20cf5d2cbad
congly <daocongly0510@gmail.com>
2022-06-18 18:08:17 +0700

    ok

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -5,7 +5,7 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
 
 const required =(val) => val && val.length;
 const maxLength = (len) => (val)=> !(val) || (val.length <= len);
-const minLength = (len) => (val)=> (val) || (val.length >= len);
+const minLength = (len) => (val)=> (val) && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
@@ -53,7 +53,7 @@ class Contact extends React.Component {
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
                             <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-info" href="mailto:confusion@food.net"><i className="fa fa-skype"></i> Skype</a>
                             <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
                     </div>
