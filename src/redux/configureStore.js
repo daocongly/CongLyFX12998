1a6aa569762c1f34c9302bc0076dd288e33b04fa
congly <daocongly0510@gmail.com>
2022-06-18 10:54:46 +0700

    copy lab08_01

---
+++ src/redux/configureStore.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers } from "redux";
+import {Dishes} from "./dishes";
+import {Leaders} from "./leaders";
+import {Promotions} from "./promotions";
+import {Comments} from "./comments";
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+
+        })
+
+    );
+    return store;
+};
