101769348842963ef00149f47d0d5b806f2760f9
congly <daocongly0510@gmail.com>
2022-06-19 10:10:35 +0700

    11_50

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -10,3 +10,23 @@ export const addComment = (dishId, rating, author, comment)=>({
         comment: comment
     }
 });
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    },2000)
+}
+
+export const dishesLoading =()=> ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+})
+
+export const addDishes = (dishes) => ({
+    
+})
