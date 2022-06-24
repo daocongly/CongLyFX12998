9c9fcfe01183927f94a0351936c27b1930b0a146
congly <daocongly0510@gmail.com>
2022-06-25 00:35:24 +0700

    chua xong sai

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,8 @@ import dateFormat from 'dateformat';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm,Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 
 const maxLength = (len)=>(val)=>!(val) || (val.length <=len);
 const minLength = (len)=>(val)=> val && (val.length >=len)
@@ -13,7 +15,7 @@ function RenderDish({dish}) {
     return (
         <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src={dish.image}  alt={dish.name}></CardImg>
+                <CardImg top src={baseUrl + dish.image}  alt={dish.name}></CardImg>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
