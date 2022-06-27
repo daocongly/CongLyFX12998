f080fc9b712524229d42d021b6ebdf210914f039
congly <daocongly0510@gmail.com>
2022-06-27 23:51:51 +0700

    oki ê

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -18,6 +18,10 @@ function RenderCard({item, isLoading,errMess}) {
     }
     else
         return (
+            <FadeTransform in
+            transformProps ={{
+                exitTransform : 'scale(0.5) translateY(-50%)'
+            }}>
             <Card>
                 <CardImg src={baseUrl + item.image} alt={item.name}/>
                 <CardBody>
@@ -26,6 +30,7 @@ function RenderCard({item, isLoading,errMess}) {
                     <CardText>{item.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
 
 }
