2eb61e288d5d4ab2f5b6b2e48e4261d5e1d34eae
congly <daocongly0510@gmail.com>
2022-06-27 22:24:16 +0700

    complete

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {postComment, fetchDishes, fetchComments, fetchPromos} from '../redux/ActionCreators';
 import {actions} from 'react-redux-form';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 const mapStateToProps = state => {
   return {
@@ -65,13 +66,17 @@ class Main extends Component {
     return (
       <div>
         <Header/>
-        <Switch>
-          <Route exact path="/menu" component ={() => <Menu dishes={this.props.dishes} />}/>
-          <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component = {()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
-          <Route path="/" component = {Homepage} />
-          <Redirect to="/" />
-        </Switch>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+          <Switch>
+            <Route exact path="/menu" component ={() => <Menu dishes={this.props.dishes} />}/>
+            <Route path="/menu/:dishId" component={DishWithId} />
+            <Route exact path="/contactus" component = {()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+            <Route path="/" component = {Homepage} />
+            <Redirect to="/" />
+          </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer/>
       </div>
     );
