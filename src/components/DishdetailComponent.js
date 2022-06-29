67af11159da228720550c76f83e3dc8fdf427d68
congly <daocongly0510@gmail.com>
2022-06-29 10:27:24 +0700

    copy lab12_01

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb,BreadcrumbItem,
+Label,Modal, ModalHeader, ModalBody,Button,Row,Col} from 'reactstrap';
+import dateFormat from 'dateformat';
+import {Link} from 'react-router-dom';
+import {Control, LocalForm,Errors} from 'react-redux-form';
+import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
+
+
+const maxLength = (len)=>(val)=>!(val) || (val.length <=len);
+const minLength = (len)=>(val)=> val && (val.length >=len)
+
+function RenderDish({dish}) {
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <FadeTransform in
+                transformProps ={{
+                exitTransform : 'scale(0.5) translateY(-50%)'
+            }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image}  alt={dish.name}></CardImg>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </FadeTransform>
+        </div>
+    );
+}
+function RenderComments({comments, postComment, dishId}){
+    if (comments != null)
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className ="list-unstyled">
+                    <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author},{dateFormat(comment.date,"mmm d, yyyy")}</p>
+                            </li>
+                            </Fade>
+                        );
+                    })}
+                    </Stagger>
+                </ul>
+                <CommentForm dishId={dishId} postComment={postComment}/>
+            </div>
+        );
+    else
+        return(
+            <div></div>
+        );       
+}
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state={
+            isNavOpen: false,
+            isModalOpen: false
+        };
+    }
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleSubmit(values){
+        this.toggleModal();
+        this.props.postComment(this.props.dishId,values.rating,values.author,values.comment);
+    }
+    render(){
+        return(
+        <div>
+            <Button outline onClick={this.toggleModal}><i class="fa fa-pencil" aria-hidden="true"></i>Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle = {this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values)=> this.handleSubmit(values)}>
+                <Row className="form-group">
+                    <Col>
+                    <Label htmlFor="rating">Rating</Label>
+                    <Control.select model=".rating" id="rating">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Control.select>
+                    </Col>
+                </Row>
+                <Row className="form-group">
+                    <Col>
+                    <Label htmlFor="author">Your Name</Label>
+                    <Control.text model=".author" id="author"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                        minLength: minLength(3),
+                        maxLength: maxLength(15)
+                    }}
+                    />
+                     <Errors
+                        className="text-danger"
+                        model=".author"
+                        show="touched"
+                        messages = {{
+                        minLength: "Must be greater than 3 number",
+                        maxLength: "Must be 15 number or less",
+                        }}
+                        />
+                    </Col>
+                </Row>
+                <Row className="form-group">
+                    <Col>
+                    <Label htmlFor="comment">Comment</Label>
+                    <Control.textarea model=".comment" id="comment"
+                    rows="6" className="form-control"
+                    />
+                    </Col>
+                </Row>
+                <Button type="submit" className = "bg-primary">Submit</Button>
+                </LocalForm>
+            </ModalBody>
+            </Modal>
+        </div>
+        );
+    }
+}
+
+const DishDetail = (props) => {
+    if(props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading/>
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess){
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish !=null)
+    return(
+        <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                </div>
+            <div className="row">              
+                <RenderDish dish ={props.dish}/>
+                <RenderComments comments={props.comments}
+                postComment={props.postComment}
+                dishId={props.dish.id}
+                />
+            </div>
+        </div>
+    );
+    else
+    return(
+        <div></div>
+    );
+}
+
+export default DishDetail;
