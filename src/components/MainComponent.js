3e5252aca89267f88110c39de150ef2d5c2cac55
congly <daocongly0510@gmail.com>
2022-06-29 10:33:10 +0700

    lab11_02 ok

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -27,9 +27,7 @@ const mapDispatchToProps = (dispatch) => ({
   fetchPromos: () => {dispatch(fetchPromos())},
 })
 class Main extends Component {
-  constructor(props){
-    super(props);
-  }
+
 
   componentDidMount(){
     this.props.fetchDishes();
