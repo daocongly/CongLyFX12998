c34d7eb715efaed7c0ffffdda9b170f0bdb0f776
congly <daocongly0510@gmail.com>
2022-06-29 11:19:24 +0700

    fix error ok

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Department(props){
-    const depart = props.depart.map((item) =>{
+    const depart = props.depart.map((item,i) =>{
         return(
-            <div className="card-header border col-sm-12 col-md-6 col-lg-4">
+            <div key={i} className="card-header border col-sm-12 col-md-6 col-lg-4">
                    <h3>{item.name}</h3>
                    <p>Số lượng nhân viên: {item.numberOfStaff}</p>
             </div>
